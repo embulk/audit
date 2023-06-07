f0034e90760aa8efb60e8f8cf82508969f891d32
Dai MIKURUBE <dmikurube@acm.org>
2023-06-07 16:02:27 +0900

    Assert with the set of repos

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@
 
 source "https://rubygems.org"
 
+gem 'minitest', '~> 5.18.0'
 gem 'octokit', '~> 6.1'
--- audit.rb
+++ audit.rb
@@ -1,9 +1,31 @@
 # coding: utf-8
 require 'octokit'
+require 'minitest'
+require 'yaml'
 
-client = Octokit::Client.new(access_token: ENV.fetch('GITHUB_TOKEN'))
+include Minitest::Assertions
 
-query = <<-GRAPHQL
+class << self
+  attr_accessor :assertions
+end
+self.assertions = 0
+
+def assert_repos(expected_repos, actual_repos)
+  expected_repo_names = expected_repos.map { |node| node[:name] }
+  actual_repo_names = actual_repos.map { |node| node[:name] }
+  assert_equal expected_repo_names, actual_repo_names
+end
+
+def load_expected_repos(repos_yaml)
+  return File.open(repos_yaml) do |f|
+    YAML.load(f, filename: repos_yaml, symbolize_names: true)
+  end
+end
+
+def load_actual_repos(access_token)
+  client = Octokit::Client.new(access_token: access_token)
+
+  query = <<-GRAPHQL
 query paginate($cursor: String) {
   organization(login: "embulk") {
     repositories(first: 100, orderBy: {field: NAME, direction: ASC}, after: $cursor) {
@@ -19,7 +41,11 @@ query paginate($cursor: String) {
 }
 GRAPHQL
 
-response = client.post '/graphql', { query: query }.to_json
-response[:data][:organization][:repositories][:nodes].each do |repo|
-  puts repo.attrs
+  response = client.post '/graphql', { query: query }.to_json
+  return response[:data][:organization][:repositories][:nodes]
 end
+
+expected_repos = load_expected_repos("repos.yaml")
+actual_repos = load_actual_repos(ENV.fetch('GITHUB_TOKEN'))
+
+assert_repos(expected_repos, actual_repos)
