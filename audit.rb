9142f8d2f9f0969a03fb23b8d31dca6becef5078
Dai MIKURUBE <dmikurube@acm.org>
2024-12-19 14:48:06 +0900

    Consider collaborators, and dump the actual values to actual.yaml

--- audit.rb
+++ audit.rb
@@ -15,6 +15,18 @@ class << self
 end
 self.assertions = 0
 
+def sym_to_s(value)
+  if value.is_a?(Hash)
+    value.map { |k, v| [ k.to_s, sym_to_s(v) ] }.to_h
+  elsif value.is_a?(Array)
+    value.map { |e| sym_to_s(e) }
+  elsif value.is_a?(Symbol)
+    value.to_s
+  else
+    value
+  end
+end
+
 def retrieve_github_token()
   token_path = SCRIPT_PATH.join('github_token')
   if File.exist?(token_path)
@@ -51,6 +63,12 @@ query paginate($cursor: String) {
     repositories(visibility: PUBLIC, first: 100, orderBy: {field: NAME, direction: ASC}, after: $cursor) {
       nodes {
         name
+        collaborators {
+          totalCount
+          nodes {
+            login
+          }
+        }
         visibility
       }
       pageInfo {
@@ -66,12 +84,20 @@ GRAPHQL
   repositories_array = response_sawyer.data.to_h[:organization][:repositories][:nodes]
   repositories = {}
   repositories_array.each do |repository|
-    repositories[repository[:name]] = {}
+    repositories[repository[:name]] = sym_to_s(repository)
   end
   return repositories
 end
 
+def dump_actual_repos(actual_repos, io)
+  YAML.dump(actual_repos, io)
+end
+
 expected_repos = load_expected_repos("repos.yaml")
 actual_repos = load_actual_repos(retrieve_github_token())
+
+File.open("actual.yaml", "w") do |f|
+  dump_actual_repos(actual_repos, f)
+end
 
 assert_repos(expected_repos, actual_repos)
